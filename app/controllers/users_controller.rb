801f299858b989cae90f55ab6c18247d71d39c62
Vladislav Lewinn <vlewin@suse.de>
2014-03-28 13:55:21 +0100

    pundit integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,46 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  # after_action :verify_authorized #, except: [:show]
 
   def index
-    @users = User.all
+    @users = policy_scope(User)
+    authorize @users
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = policy_scope(User).find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @user = policy_scope(User).find(params[:id])
+    authorize @user
+
+    if @user.update_attributes(user_params)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
+
+  def destroy
+    user = policy_scope(User).find(params[:id])
+    authorize user
+
+    unless user == current_user
+      user.destroy
+      redirect_to users_path, :notice => "User deleted."
+    else
+      redirect_to users_path, :notice => "Can't delete yourself."
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
   end
 
 end
