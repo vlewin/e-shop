80dff7ed7fbfe5c54f477f0fbac5ecb793ab87de
Vladislav Lewinn <vlewin@suse.de>
2014-04-01 12:57:12 +0200

    adapt routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 OptimHandel::Application.routes.draw do
-  resources :categories
-
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
 
+  resources :categories
   resources :products
-  resources :users, :only => :show
-
-  namespace :admin do
-    get '', to: 'dashboard#index', as: '/'
-    resources :products
-    resources :users
-    resources :categories
-  end
+  resources :users
 end
