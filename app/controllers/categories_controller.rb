c45e63f2cd642b4066a13c2847cf9dd99afd6597
Vladislav Lewinn <vlewin@suse.de>
2014-04-01 12:58:00 +0200

    add views and controller for categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,58 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  after_action :verify_authorized, except: [:index, :show]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+    authorize @category
+  end
+
+  def edit
+    authorize @category
+  end
+
+  def create
+    @category = Category.new(category_params)
+    authorize @category
+
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    authorize @category
+
+    if @category.update(category_params)
+      redirect_to @category, notice: 'Category was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    authorize @category
+    @category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
