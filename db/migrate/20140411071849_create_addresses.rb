1ca043686b5f19f1c18d688bca1f97e5a158c459
Vladislav Lewinn <vlewin@suse.de>
2014-04-11 10:17:59 +0200

    introduce address

---
+++ db/migrate/20140411071849_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration
+  def change
+    create_table :addresses do |t|
+      t.string :country
+      t.string :city
+      t.string :street
+      t.string :zip
+      t.string :phone
+      t.string :user_id
+
+      t.timestamps
+    end
+  end
+end
