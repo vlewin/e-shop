c12581f8b7f279d706e550708be29746294badf7
Vladislav Lewinn <vlewin@suse.de>
2014-04-11 10:16:44 +0200

    categories and products should accept nested attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :category
 
   validates :name, presence: true
+
+  accepts_nested_attributes_for :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  has_many :addresses
+
   def set_default_role
     self.role ||= :user
   end
