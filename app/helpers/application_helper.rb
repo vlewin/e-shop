72f0b8464abed75459e5630f195839d3d63d2455
Vladislav Lewin <vlewin@suse.de>
2014-04-15 22:58:25 +0200

    icon_link_to without text and with additional link options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def icon_link_to(text, path, icon)
-    link_to "#{content_tag :i, nil, class: "fa #{icon}"} #{text}".html_safe, path
+  def icon_link_to(text, path, options = {}, icon)
+    link_to "#{content_tag :i, nil, class: "fa #{icon}"} #{text}".html_safe, path, options
   end
 end
