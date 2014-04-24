e9657bca4e8e34068fa8569e5a536dfdc7786bd7
Vladislav Lewin <vlewin@suse.de>
2014-04-24 23:16:13 +0200

    move relations to the top

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  enum role: [:user, :admin]
-  after_initialize :set_default_role, :if => :new_record?
-
   has_many :addresses
   has_many :orders, through: :addresses
+
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
     self.role ||= :user
