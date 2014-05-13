5ec071e4a0f0afbe300940de8730b794d42300cf
Vladislav Lewinn <vlewin@suse.de>
2014-05-13 11:25:30 +0200

    fix wizard breadcrumb

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -6,13 +6,15 @@ $(function() {
     $(this).tab('show')
   })
 
-  $('#order a.btn-next').click(function (e) {
+  $('.new_order_form a.btn-next').click(function (e) {
+    console.log('Next clicked')
     e.preventDefault()
     $('.wizard a').removeClass('active')
-    $('.wizard').find('[href^=' + $(this).attr('href') + ']').addClass('active')
+    $('.wizard').find('[href=' + $(this).attr('href') + ']').addClass('active')
+    console.log($(this).attr('href'))
   })
 
-  $('#order a.btn-back').click(function (e) {
+  $('.new_order_form a.btn-back').click(function (e) {
     e.preventDefault()
     $('.wizard a').removeClass('active')
     $('.wizard').find('[href^=' + $(this).attr('href') + ']').addClass('active')
