c4b5916db800fd27e9e7936fde7d19fbbd288558
Vladislav Lewinn <vlewin@suse.de>
2014-05-20 12:29:56 +0200

    refactor image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,39 +1,32 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
+  include Cloudinary::CarrierWave
+  include CarrierWave::RMagick
 
-    process :convert => 'png'
-    # process :tags => ['product_image']
+  process :tags => ['product_image']
+  process :convert => 'png'
 
-    version :standard do
-      process :resize_to_fill => [250, 250, :north]
-    end
+  version :standard do
+    process :resize_to_fill => [250, 250]
+  end
 
-    version :thumbnail do
-      resize_to_fit(240, 180)
-    end
+  version :thumbnail, :from_version => :standard do
+    process :resize_to_fill => [240,120]
+  end
 
-    def public_id
-      return model.name
-    end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
-    def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
-    def extension_white_list
-      %w(jpg jpeg gif png pdf doc txt)
-    end
-  else
-    storage :file
+  def cache_dir
+    Rails.root.join('tmp/cache')
+  end
 
-    def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
-
-    def extension_white_list
-      %w(jpg jpeg gif png pdf doc txt)
-    end
+  def public_id
+    return model.name
   end
 end
 
