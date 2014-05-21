08b320c60a1ff80ec688ea4d48dd63f2ced0ea7c
Vladislav Lewinn <vlewin@suse.de>
2014-05-21 15:36:00 +0200

    decrease product quantity after article was bought by a customer

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,6 +15,7 @@ class Order < ActiveRecord::Base
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       line_item = item.dup
+      line_item.product.decrease_quantity line_item.quantity
       line_item.cart_id = nil
       line_item.save!
       line_items << line_item
