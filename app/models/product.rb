1800a47027c42f71f9fb5eecafc151e4a6f74dd6
Vladislav Lewin <vlewin@suse.de>
2014-07-09 09:27:39 +0200

    clean up

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ActiveRecord::Base
 
   belongs_to :category
   has_many :line_items
-  self.
   has_many :orders, through: :line_items
 
   before_destroy :ensure_not_referenced_by_any_line_item
