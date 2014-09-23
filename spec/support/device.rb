0a3df98767e90b7edfd6ccf38ff90f348df21562
Vladislav Lewin <vlewin@suse.de>
2014-09-23 12:29:33 +0200

    enable devise support in RSPEC

--- spec/support/device.rb
+++ spec/support/device.rb
@@ -1,3 +1,21 @@
+module ControllerMacros
+  def login_admin
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in FactoryGirl.create(:admin)
+    end
+  end
+
+  def login_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.create(:user)
+      sign_in user
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
 end
