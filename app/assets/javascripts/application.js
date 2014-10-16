e719aae3ebe2724eae2bbfc0c8e0f0b5d542706c
Vladislav Lewin <vlewin@users.noreply.github.com>
2014-10-16 23:11:57 +0200

    ui improvments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,16 @@
 //= require twitter/bootstrap
 //= require_tree
 
+$( "body" ).on( "click", ".quantity-helper button", function() {
+  var $input = $(this).parents('.quantity-helper').find('input');
+  var quantity = parseInt($input.val())
 
+  if($(this).hasClass('quantity_minus')) {
+    quantity = (quantity == 0 || quantity == 1) ? 1 : quantity-1
+  } else {
+    quantity = (quantity+1 > $input.data('max')) ? quantity  : quantity+1
+  }
+
+  $input.val(quantity)
+  return false;
+});
--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -5,17 +5,4 @@ $(function() {
       $form.submit()
     }, 1000)
   });
-
-  $( "body" ).on( "click", ".quantity-helper button", function() {
-    var $input = $(this).parents('.quantity-helper').find('input');
-    var quantity = parseInt($input.val())
-
-    if($(this).hasClass('quantity_minus')) {
-      quantity = (quantity == 0 || quantity == 1) ? 1 : quantity-1
-    } else {
-      quantity = (quantity+1 > $input.data('max')) ? quantity  : quantity+1
-    }
-
-    $input.val(quantity)
-  });
 });
