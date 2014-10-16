f96466b046e64f556bbc6a29aaf6a969508c1212
Vladislav Lewin <vlewin@users.noreply.github.com>
2014-10-16 22:17:41 +0200

    ruby 1.9 hash syntax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,13 +12,13 @@ module ApplicationHelper
   end
 
   # FIXME: Refactoring
-  def product_image_tag(product, version = :standard, options = {})
+  def product_image_tag(product, version = :standard, options = { class: 'img-responsive'})
     if product.image.url
       # if Rails.env.production?
         if version ==:standard
-          cl_image_tag product.image, {:width => 250, :height => 250, :crop => :fill}.merge(options)
+          cl_image_tag product.image, {width: 250, height: 250, crop: :fill}.merge(options)
         else
-          cl_image_tag product.image, {:width => 280, :height => 140, :crop => :fill}.merge(options)
+          cl_image_tag product.image, {width: 240, height: 200, crop: :fill}.merge(options)
         end
       # else
         # image_tag product.image.send(version).url, options
@@ -30,7 +30,7 @@ module ApplicationHelper
         if version ==:standard
           image_tag "http://placehold.it/250x250", options
         else
-          image_tag "http://placehold.it/280x140", options
+          image_tag "http://placehold.it/240x200", options
         end
       end
     end
@@ -41,16 +41,16 @@ module ApplicationHelper
   end
 
   def quantity_field_tag(quantity, options = {})
-    content_tag(:div, nil, :class => 'input-group input-group-sm quantity-helper') do
+    content_tag(:div, nil, class: 'input-group input-group-sm quantity-helper') do
       concat(
-        content_tag(:span, nil, :class => 'input-group-btn') do
+        content_tag(:span, nil, class: 'input-group-btn') do
           button_tag('-', type: 'button', class: 'btn btn-default quantity_minus')
         end
       )
 
       concat(text_field_tag :quantity, quantity, class: 'form-control input-disabled text-center', 'data-max' => options[:max])
       concat(
-        content_tag(:span, nil, :class => 'input-group-btn') do
+        content_tag(:span, nil, class: 'input-group-btn') do
           concat button_tag('+', type: 'button', class: 'btn btn-default quantity_plus')
         end
       )
@@ -60,9 +60,9 @@ module ApplicationHelper
   def order_status(order)
     labels = { 0 => 'label-default', 1 => 'label-default', 2 => 'label-primary', 3 => 'label-success'}
     icons = { 0 => 'fa-archive', 1 => 'fa-clock-o', 2 => 'fa-truck', 3 => 'fa-check-circle'}
-    content_tag(:span, nil, :class => "label #{labels[order[:status]]}") do
+    content_tag(:span, nil, class: "label #{labels[order[:status]]}") do
       concat(
-        content_tag(:i, nil, :class => "fa #{icons[order[:status]]}")
+        content_tag(:i, nil, class: "fa #{icons[order[:status]]}")
       )
 
       concat ' ' + order.status.humanize
