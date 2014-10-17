2f9df5a87a4a0ca8f49cc2d890acf1f018e2bd0e
Vladislav Lewin <vlewin@suse.de>
2014-10-17 13:09:58 +0200

    add active class to the filter links (category, view, sorting)

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,4 +1,11 @@
 $(function() {
+  // Categories
+  $('body').on('click', '#categories li>a', function() {
+    $('#categories li').removeClass('active')
+    $(this).parent().addClass('active')
+  })
+
+  // Search
   $("#product_search input").on('keyup', function(){
     var $form = $(this);
     setTimeout(function(){
