554314782fafd7ee19a5a9c63f41621a04503bae
Vladislav Lewin <vlewin@suse.de>
2014-10-20 15:58:36 +0200

    remove locale routes scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,25 @@
 EShop::Application.routes.draw do
   root to: 'store#index', via: :get
-
-  scope '(:locale)', locale: /en|de/ do
-    devise_for :users, controllers: { registrations: :account }
-    devise_scope :user do
-      resource :account, controller: :account do
-      end
+  devise_for :users, controllers: { registrations: :account }
+  devise_scope :user do
+    resource :account, controller: :account do
     end
+  end
 
-    resources :users
-    resources :addresses
-    resources :categories
-    resources :orders
-    resources :carts
-    resources :line_items
-    resources :delivery_services
+  resources :users
+  resources :addresses
+  resources :categories
+  resources :orders
+  resources :carts
+  resources :line_items
+  resources :delivery_services
 
-    resource :store,  only: [:index] do
-      resources :products, controller: :store, only: [:index, :show] do
-      end
+  resource :store,  only: [:index] do
+    resources :products, controller: :store, only: [:index, :show] do
     end
+  end
 
-    resources :products do
-      collection { get :search, to: 'products#index' }
-    end
+  resources :products do
+    collection { get :search, to: 'products#index' }
   end
 end
