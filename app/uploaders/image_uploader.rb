f5b456f021c7fbfcf49f4c7d0045e4c075a6c1bf
Vladislav Lewin <vlewin@suse.de>
2014-10-21 13:57:17 +0200

    re-factor image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,27 +2,26 @@ class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   include CarrierWave::RMagick
 
-  process :tags => ['product_image']
-  process :convert => 'png'
+  process tags: ['product_image']
+  process convert: 'png'
 
-  version :standard do
-    process :resize_to_fill => [250, 250]
+  version :show do
+    process eager: true
+    process resize_to_fill: [250, 250]
   end
 
-  version :thumbnail, :from_version => :standard do
-    process :resize_to_fill => [240,120]
+  version :index, from_version: :show do
+    process eager: true
+    process resize_to_fill: [250, 200]
+  end
+
+  version :thumbnail, from_version: :show do
+    process eager: true
+    process resize_to_fill: [50,50]
   end
 
   def extension_white_list
     %w(jpg jpeg gif png)
-  end
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  def cache_dir
-    Rails.root.join('tmp/cache')
   end
 
   def public_id
