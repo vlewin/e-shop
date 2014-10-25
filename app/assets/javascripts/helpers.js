d3c210f3030fb248ba98ea167735bda6d13fefc1
Vladislav Lewin <vlewin@users.noreply.github.com>
2014-10-25 13:23:21 +0200

    fix progress steps alignment and switch issue

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -5,3 +5,7 @@ Array.prototype.first = function () {
 Array.prototype.last = function () {
   return this.pop();
 };
+
+Array.prototype.diff = function(a) {
+  return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
