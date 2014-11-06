f988e97f32095c03d4d7814e52fc98fdea598953
Vladislav Lewin <vlewin@suse.de>
2014-11-06 08:44:14 +0100

    shoulda-matcher methods not found in Rails 4.1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'shoulda-matchers'
 require 'pundit/rspec'
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'shoulda-matchers'
 require 'simplecov'
 SimpleCov.start
 
