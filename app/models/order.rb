0a69f208922c26608bc30807a0b20dcdea0ff46d
Vladislav Lewin <vlewin@users.noreply.github.com>
2014-11-06 21:46:57 +0100

    disable policy check for new and create

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,6 +13,7 @@ class Order < ActiveRecord::Base
 
   belongs_to :address, foreign_key: :shipping_address_id
   belongs_to :billing_address, class_name: 'Address', foreign_key: :billing_address_id
+  belongs_to :shipping_address, class_name: 'Address', foreign_key: :shipping_address_id
 
   has_many :line_items, dependent: :destroy
 
--- app/policies/address_policy.rb
+++ app/policies/address_policy.rb
@@ -1,26 +1,40 @@
 class AddressPolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      if user.admin?
+        scope.unscoped.all
+      else
+        user.addresses
+      end
+    end
+  end
+
   def index?
-    @user.admin?
+    @user.admin? || @user.addresses.active.exists?(record)
   end
 
   def show?
-    @user.admin? || @user.addresses.exists?(record)
+    @user.admin? || @user.addresses.active.exists?(record)
   end
 
-  def new?
-    @user.admin?
-  end
+  # def new?
+  #   @user.admin?
+  # end
 
-  def create?
-    @user.admin?
-  end
+  # def create?
+  #   @user.admin? || @user.addresses.exists?(record)
+  # end
 
   def edit?
-    @user.admin?
+    @user.admin? || @user.addresses.active.exists?(record)
   end
 
   def update?
-    @user.admin?
+    @user.admin? || @user.addresses.active.exists?(record)
+  end
+
+  def delete?
+    @user.admin? || @user.addresses.active.exists?(record)
   end
 
   def destroy?
