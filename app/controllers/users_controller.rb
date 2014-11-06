0f98af8ba601d09faa8a61d7e54993531cd8dd11
Vladislav Lewin <vlewin@suse.de>
2014-11-06 13:04:47 +0100

    add destroy action to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,15 @@ class UsersController < ApplicationController
     @user = current_user.admin? ? User.find(params[:id]) : current_user
     authorize current_user
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    authorize @user
+
+    if @user.destroy
+      redirect_to users_path
+    else
+      redirect_to users_path, alert: "#{@product.errors.full_messages.join(', ')}"
+    end
+  end
 end
