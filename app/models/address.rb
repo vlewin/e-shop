6c08d010f5b0cc4d35bbdc76fb6be6e4ac86772c
Vladislav Lewin <vlewin@users.noreply.github.com>
2014-11-06 21:45:27 +0100

    add status enums and status scopes to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,11 @@ class Address < ActiveRecord::Base
 
   validates :recipient, :city, :street, :zip_code, :phone, :user_id,  presence: true
 
+  enum status: { active: 0, inactive: 1 }
+
+  scope :active, -> { where(status: statuses[:active]) }
+  scope :inactive, -> { where(status: statuses[:inactive]) }
+
   def full_address
     "#{recipient}, #{street}, #{zip_code} #{city}"
   end
