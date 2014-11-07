d193472668d70dad2df3233e758e832be77a18d7
Vladislav Lewin <vlewin@suse.de>
2014-11-07 09:31:57 +0100

    push updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106074850) do
+ActiveRecord::Schema.define(version: 20141028203225) do
 
   create_table "addresses", force: true do |t|
     t.string   "recipient"
