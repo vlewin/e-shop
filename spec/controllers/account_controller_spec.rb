20a8f612887215d78cb2ffc5d6f1c0eb79dfa760
Vladislav Lewin <vlewin@suse.de>
2014-11-10 09:18:59 +0100

    improve account_controller test coverage

--- spec/controllers/account_controller_spec.rb
+++ spec/controllers/account_controller_spec.rb
@@ -1,21 +1,40 @@
 require 'rails_helper'
 
 describe AccountController, type: :controller do
-  login_user
-
   it { should use_before_filter(:authenticate_user!) }
   it { should use_after_action(:verify_authorized) }
 
-  describe "GET #show" do
-    it "assigns the current_user to @user" do
-      get :show
-      expect(assigns(:user)).to eq(subject.current_user)
-      expect(assigns(:addresses)).to eq(subject.current_user.addresses)
+  context 'guest' do
+    describe "GET #show" do
+      it "redirects to sign_in path" do
+        get :show
+        expect(response).to redirect_to new_user_session_path
+      end
     end
+  end
 
-    it "renders the :account view" do
-      get :show
-      expect(response).to render_template :show
+  context 'user' do
+    login_user
+
+    describe "GET #show" do
+      it "assigns the current_user to @user and renders the :account view" do
+        get :show
+        expect(assigns(:user)).to eq(subject.current_user)
+        expect(assigns(:addresses)).to eq(subject.current_user.addresses)
+        expect(response).to render_template :show
+      end
+    end
+  end
+
+  context 'admin' do
+    login_admin
+    describe "GET #show" do
+      it "assigns the current_user to @user and renders the :account view" do
+        get :show
+        expect(assigns(:user)).to eq(subject.current_user)
+        expect(assigns(:addresses)).to eq(subject.current_user.addresses)
+        expect(response).to render_template :show
+      end
     end
   end
 end
