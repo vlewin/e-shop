8b526f7f1d110d917c03e83b8f6b8a197075e37d
Vladislav Lewin <vlewin@suse.de>
2015-02-20 11:16:09 +0100

    remvoe commented code

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,8 +9,6 @@ class Product < ActiveRecord::Base
   has_many :line_items
   has_many :orders, through: :line_items
 
-  # has_many :translations, dependent: :destroy
-
   validates :title, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
